f4f5549a9055b5b978a900bd8b0252ce715d9b23
Romo_Guillermo <l19100251@nlaredo.tecnm.mx>
2021-09-16 13:40:04 -0500

    secuencia cambios

--- TrabajoJava/SerieDeNumeros/src/App.java
+++ TrabajoJava/SerieDeNumeros/src/App.java
@@ -1,27 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        //GUILLERMO ROMO VERA #19100251
         int a=1;
         int b=2;
         int c=1;
         int r=0;
+        int s=1;
 
-        System.out.println("" + a);
-        System.out.println("" + b);
-
-        r=a+b;
+        System.out.println("El primer numero es: " + a);
+        System.out.println("El segundo numero es: " + b);
+        System.out.println("");
         
         do
         {
             r=a+b;
-            System.out.println("" + r);
+            System.out.println("El resultado de la suma de la secuencia " + s + " es: " + r);
             a=b;
             b=r;
             c++;
+            s++;
         }
         while (c <= 10);
         {
-            System.out.print("Termina la secuencia");
+            System.out.print("\n" + "Termina la secuencia");
         }
 
     }
